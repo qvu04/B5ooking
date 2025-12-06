afc4292c47f546f223d9c29ddbfbea9f2a28087d
quangvungo <quangvu221004@gmail.com>
2025-12-06 15:07:29 +0700

    update

--- be/src/cron/statusBooking.js
+++ be/src/cron/statusBooking.js
@@ -1,8 +1,8 @@
 import cron from "node-cron";
-import { userService } from "../services/userService";
+import { userService } from "../services/userService.js";
 
 export const startStatusBooking = () => {
-   cron.schedule("* * * * *", async () => {
+  cron.schedule("* * * * *", async () => {
     console.log("⏰ Bắt đầu cron check...");
     await userService.statusBooking();
     console.log("✅ Cron check xong");
